066e55a71c2d6b764a1560e62de31442b68e9323
Toru Choi <blueocean239@gmail.com>
2020-08-14 16:36:21 +0800

    remove service locator, and modify codes to work with Hilt

--- app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -22,10 +22,10 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class LogApplication : Application() {
 
-    lateinit var serviceLocator: ServiceLocator
-
-    override fun onCreate() {
-        super.onCreate()
-        serviceLocator = ServiceLocator(applicationContext)
-    }
+//    lateinit var serviceLocator: ServiceLocator
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        serviceLocator = ServiceLocator(applicationContext)
+//    }
 }
